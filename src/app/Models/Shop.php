a6cbc17ff19189599fd85dee2c49ab4fc8941a07
takuya178 <takuya.u1005@gmail.com>
2022-06-18 10:23:23 +0900

    Shop作成トランザクション Owner作成した時に同時にShopも作成される

--- src/app/Models/Shop.php
+++ src/app/Models/Shop.php
@@ -10,6 +10,15 @@ class Shop extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'owner_id',
+        'name',
+        'information',
+        'filename',
+        'is_selling'
+    ];
+
+
     public function Owner()
     {
         return $this->belongsTo(Owner::class);
