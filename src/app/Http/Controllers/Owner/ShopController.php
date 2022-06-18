01b3cdff739bc35c6ac36a85b7eff52711181472
takuya178 <takuya.u1005@gmail.com>
2022-06-18 19:08:50 +0900

    Shopコントローラミドルウェア。編集やアップデートページに他のユーザーが見れないように設定

--- src/app/Http/Controllers/Owner/ShopController.php
+++ src/app/Http/Controllers/Owner/ShopController.php
@@ -12,6 +12,19 @@ class ShopController extends Controller
     public function __construct()
     {
         $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+           $id = $request->route()->parameter('shop'); // indexに接続するとnullが返ってくる
+            if (!is_null($id)) {// null判定 
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId; // 文字列->数値に型変換
+                $ownerId = Auth::id();
+                if ($shopId !== $ownerId) {
+                    abort(404);
+                }
+            } 
+            return $next($request);
+        });
     }
 
     public function index()
@@ -25,7 +38,7 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-
+        dd(Shop::findOrFail($id));
     }
 
     public function update(Request $request, $id)
